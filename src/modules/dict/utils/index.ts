2d0e188f4e48626f5fd555230e737be439995191
黄世永 <huangsy38@chinatelecom.cn>
2024-11-20 14:47:03 +0800

    start

---
+++ src/modules/dict/utils/index.ts
@@ -0,0 +1,21 @@
+export function deepFind(value: any, list: any[]) {
+	function deep(arr: any[]): any | undefined {
+		for (const e of arr) {
+			if (e.value === value) {
+				return e;
+			} else if (e.children) {
+				const d = deep(e.children);
+				if (d !== undefined) {
+					return d;
+				}
+			}
+		}
+		return undefined;
+	}
+
+	return deep(list);
+}
+
+export function isEmpty(val: any) {
+	return val === '' || val === null || val === undefined;
+}
